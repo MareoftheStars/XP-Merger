856ca351d18f0853f503001144ed8a2b3345916e
MareoftheStars <diez.sean.pro@gmail.com>
2023-03-23 21:02:08 +0100

    First Commit - general pre-config
    1.19.3-44.1.23
    
    Pre-Configured build.gradle and such

---
+++ src/main/java/com/mareofthestars/xpmerger/XPMerger.java
@@ -0,0 +1,11 @@
+package com.mareofthestars.xpmerger;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(XPMerger.MODID)
+public class XPMerger
+{
+    // Define mod id in a common place for everything to reference
+    public static final String MODID = "xpmerger";
+}
